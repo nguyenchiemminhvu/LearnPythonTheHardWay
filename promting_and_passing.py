bb38dee098154832974620c92a29ee8c625be2c9
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2017-05-27 14:23:47 +0700

    update

---
+++ promting_and_passing.py
@@ -0,0 +1,12 @@
+# God is with us
+
+from sys import argv;
+
+script, user = argv;
+promt = "> ";
+
+print "Hi %s, I'm %s script" % (user, script);
+
+print "";
+print "Do you love Jesus?";
+answer = raw_input(promt);
